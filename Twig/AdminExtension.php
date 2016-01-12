7585983cf2de10c27f88b00ceb39152ad208ab29
Victor Yagnov <vyagnov@gmail.com>
2016-01-12 22:24:52 +0300

    json pront recursive

--- Twig/AdminExtension.php
+++ Twig/AdminExtension.php
@@ -16,6 +16,7 @@ class AdminExtension extends Twig_Extension
     {
         return array(
             new \Twig_SimpleFunction('admin_json_decode', array($this, 'jsonDecode'), array('is_safe' => array('html'))),
+            new \Twig_SimpleFunction('is_array', array($this, 'isArray'), array('is_safe' => array('html'))),
         );
     }
 
@@ -26,6 +27,14 @@ class AdminExtension extends Twig_Extension
 
     public function jsonDecode($json){
         return json_decode($json,true);
+    }
+
+    public function isArray($val){
+        return is_array($val);
+    }
+
+    public function printData($data){
+
     }
 
 }
